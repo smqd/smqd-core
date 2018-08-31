e77e1016d250ea45173453a08fdfb57fa2deef19
Kwon, Yeong-Eon <eirny@uangel.com>
2018-08-31 10:50:20 +0900

    bas using duplex handler instead of shaping handler

--- src/main/scala/com/thing2x/smqd/net/mqtt/MqttBpsCounter.scala
+++ src/main/scala/com/thing2x/smqd/net/mqtt/MqttBpsCounter.scala
@@ -16,8 +16,7 @@ package com.thing2x.smqd.net.mqtt
 
 import io.netty.buffer.{ByteBuf, ByteBufHolder}
 import io.netty.channel.ChannelHandler.Sharable
-import io.netty.channel.{ChannelHandlerContext, ChannelPromise}
-import io.netty.handler.traffic.{ChannelTrafficShapingHandler, TrafficCounter}
+import io.netty.channel.{ChannelDuplexHandler, ChannelHandlerContext, ChannelPromise}
 
 /**
   * 2018. 5. 30. - Created by Kwon, Yeong Eon
@@ -29,7 +28,7 @@ object MqttBpsCounter {
 
 @Sharable
 class MqttBpsCounter(enableThrottling: Boolean, readLimit: Long, checkInterval: Long, maxTime: Long)
-  extends ChannelTrafficShapingHandler(0, readLimit, checkInterval, maxTime) {
+  extends ChannelDuplexHandler {
 
   override def channelRead(ctx: ChannelHandlerContext, msg: scala.Any): Unit = {
     val bytes = msg match {
@@ -41,10 +40,10 @@ class MqttBpsCounter(enableThrottling: Boolean, readLimit: Long, checkInterval: 
     val metrics = ctx.channel.attr(ATTR_METRICS).get
     metrics.byteReceived.inc(bytes)
 
-    super.channelRead(ctx, msg)
+    ctx.fireChannelRead(msg)
   }
 
-  override def write(ctx: ChannelHandlerContext, msg: Any, promise: ChannelPromise): Unit = {
+  override def write(ctx: ChannelHandlerContext, msg: scala.Any, promise: ChannelPromise): Unit = {
     val bytes = msg match {
       case msg: ByteBuf => msg.readableBytes()
       case msg: ByteBufHolder => msg.content().readableBytes()
@@ -54,11 +53,6 @@ class MqttBpsCounter(enableThrottling: Boolean, readLimit: Long, checkInterval: 
     val metrics = ctx.channel.attr(ATTR_METRICS).get
     metrics.byteSent.inc(bytes)
 
-    super.write(ctx, msg, promise)
+    ctx.write(msg, promise)
   }
-
-  override def doAccounting(counter: TrafficCounter): Unit = {
-    super.doAccounting(counter)
-  }
-
 }
