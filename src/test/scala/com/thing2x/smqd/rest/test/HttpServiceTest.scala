06b29936f7d655c195b8879ddea9b5637a7c0a5a
Kwon, Yeong-Eon <eirny@uangel.com>
2018-09-28 12:30:50 +0900

    add oauth2 test case

---
+++ src/test/scala/com/thing2x/smqd/rest/test/HttpServiceTest.scala
@@ -0,0 +1,196 @@
+// Copyright 2018 UANGEL
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.thing2x.smqd.rest.test
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import akka.http.scaladsl.model.headers.{Authorization, OAuth2BearerToken}
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity, StatusCodes}
+import akka.http.scaladsl.server._
+import akka.http.scaladsl.testkit.ScalatestRouteTest
+import akka.testkit.TestKit
+import com.thing2x.smqd.net.http.HttpService
+import com.thing2x.smqd.rest.api.UserController.LoginResponse
+import com.thing2x.smqd.{Smqd, SmqdBuilder}
+import com.typesafe.config.ConfigFactory
+import com.typesafe.scalalogging.StrictLogging
+import org.scalatest.{WordSpec, _}
+import spray.json.{JsNumber, JsonParser, ParserInput}
+
+import scala.concurrent.Promise
+
+// 2018. 7. 15. - Created by Kwon, Yeong Eon
+
+
+class HttpServiceTest extends WordSpec
+  with BeforeAndAfterAll
+  with Matchers
+  with ScalatestRouteTest
+  with StrictLogging {
+
+  private val config = ConfigFactory.parseString(
+    """
+      |akka.actor.provider=local
+      |smqd {
+      |  services=["rest-test"]
+      |
+      |  rest-test {
+      |    entry.plugin="thing2x-core-http"
+      |    config: {
+      |      "cors" : {
+      |        "enabled" : true
+      |      },
+      |      "local" : {
+      |        "enabled": true
+      |        "port" : 0
+      |        "address" : "127.0.0.1"
+      |        "secure" : {
+      |            "enabled" : false
+      |        }
+      |      }
+      |      "routes" : [
+      |        {
+      |            "name" : "test"
+      |            "class" : "com.thing2x.smqd.rest.TestController"
+      |            "prefix" : "test"
+      |        },
+      |        {
+      |            "name" : "login"
+      |            "class" : "com.thing2x.smqd.rest.test.OAuthController"
+      |            "prefix" : "test"
+      |        }
+      |      ]
+      |    }
+      |  }
+      |}
+    """.stripMargin).withFallback(ConfigFactory.parseResources("smqd-ref.conf"))
+
+  override def createActorSystem(): ActorSystem = ActorSystem(actorSystemNameFrom(getClass), config)
+
+  var smqdInstance: Smqd = _
+  var routes: Route = _
+  val shutdownPromise = Promise[Boolean]
+
+  override def beforeAll(): Unit = {
+
+    smqdInstance = new SmqdBuilder(config)
+      .setActorSystem(system)
+      .build()
+
+    smqdInstance.start()
+    routes = smqdInstance.service("rest-test").get.asInstanceOf[HttpService].routes
+  }
+
+  override def afterAll(): Unit = {
+    shutdownPromise.future.onComplete { _ =>
+      smqdInstance.stop()
+      TestKit.shutdownActorSystem(system)
+    }
+  }
+
+  "TestController" must {
+    "blackhole" in {
+      Get("/test/blackhole", HttpEntity.Empty) ~> routes ~> check {
+        status shouldEqual StatusCodes.OK
+        responseAs[String] shouldEqual "OK 0 bytes received"
+      }
+    }
+
+    "echo" in {
+      Post("/test/echo") ~> routes ~> check {
+        status shouldEqual StatusCodes.OK
+        responseAs[String] shouldEqual "Hello"
+      }
+
+      Post("/test/echo/World") ~> routes ~> check {
+        status shouldEqual StatusCodes.OK
+        responseAs[String] shouldEqual "Hello World"
+      }
+    }
+  }
+
+  "OAuthController" must {
+    var token: String = ""
+    var refreshToken: String = ""
+
+    "login" in {
+      val loginReq = HttpEntity.apply(ContentTypes.`application/json`,
+        """
+          |{
+          |   "user": "admin",
+          |   "password": "password"
+          |}
+        """.stripMargin)
+
+      Post("/test/login", loginReq) ~> routes ~> check {
+        status shouldEqual StatusCodes.OK
+        // parse json response
+        val response = entityAs[String]
+        val responseJson = JsonParser(ParserInput(response)).asJsObject
+
+        // check result code from json response
+        val responseCode = responseJson.getFields("code").head.asInstanceOf[JsNumber]
+        assert(responseCode.value.intValue == 0)
+
+        // check result body as a LoginResponse
+        val loginRsp = responseJson.getFields("result").head.convertTo[LoginResponse]
+        // logger.info(s"===> ${loginRsp.access_token}")
+        token = loginRsp.access_token
+        refreshToken = loginRsp.refresh_token
+      }
+    }
+
+    "sanity" in {
+      //logger.info(s"===> token: $token")
+      Get("/test/sanity").addHeader(Authorization(OAuth2BearerToken(token))) ~> routes ~> check {
+        status shouldEqual StatusCodes.OK
+        //logger.info("============>")
+      }
+    }
+
+    "refresh" in {
+      logger.info(s"===> refresh: $refreshToken")
+      val refreshReq = HttpEntity(ContentTypes.`application/json`,
+        s"""
+          |{
+          |  "refresh_token": "$refreshToken"
+          |}
+        """.stripMargin)
+
+      Post("/test/refresh", refreshReq) ~> routes ~> check {
+        status shouldEqual StatusCodes.OK
+        val response = entityAs[String]
+        val responseJson = JsonParser(ParserInput(response)).asJsObject
+        val loginRsp = responseJson.getFields("result").head.convertTo[LoginResponse]
+        val newToken = loginRsp.access_token
+        assert(newToken != token)
+        token = newToken
+      }
+    }
+
+    "refresh_sanity" in {
+      Get("/test/sanity").addHeader(Authorization(OAuth2BearerToken(token))) ~> routes ~> check {
+        status shouldEqual StatusCodes.OK
+        //logger.info("============>")
+      }
+    }
+  }
+
+  "HttpService" must {
+    "shutdown" in {
+      shutdownPromise.success(true)
+    }
+  }
+}
---
+++ src/test/scala/com/thing2x/smqd/rest/test/OAuthController.scala
@@ -0,0 +1,75 @@
+package com.thing2x.smqd.rest.test
+
+import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.server.{Directives, Route}
+import com.thing2x.smqd.net.http.HttpServiceContext
+import com.thing2x.smqd.rest.RestController
+import com.thing2x.smqd.rest.api.UserController.{LoginRefreshRequest, LoginRefreshResponse, LoginRequest, LoginResponse}
+import com.typesafe.scalalogging.StrictLogging
+import com.thing2x.smqd.net.http.OAuth2.{OAuth2Claim, OAuth2RefreshClaim}
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import com.thing2x.smqd.SmqSuccess
+import spray.json.{JsObject, JsString}
+
+import scala.util.Success
+
+// 9/28/18 - Created by Kwon, Yeong Eon
+
+/**
+  *
+  */
+class OAuthController(name: String, context: HttpServiceContext) extends RestController(name, context)
+  with Directives with StrictLogging {
+
+  override def routes: Route = login ~ refresh ~ context.oauth2.authorized{ claim => sanity(claim) }
+
+  def login: Route = {
+    path("login") {
+      post {
+        entity(as[LoginRequest]) { loginReq =>
+          val login = context.smqdInstance.userLogin(loginReq.user, loginReq.password)
+          onComplete(login) {
+            case Success(SmqSuccess(userInfo)) =>
+              val claim = OAuth2Claim(loginReq.user, userInfo + ("issuer" -> "smqd-http-test"))
+              val refreshClaim = OAuth2RefreshClaim(loginReq.user, Map("issuer" -> "smqd-http-test"))
+              context.oauth2.issueJwt(claim, refreshClaim) { jwt =>
+                val response = LoginResponse(jwt.tokenType, jwt.accessToken, jwt.accessTokenExpire, jwt.refreshToken, jwt.refreshTokenExpire)
+                complete(StatusCodes.OK, restSuccess(0, response.toJson))
+              }
+            case _ =>
+              complete(StatusCodes.Unauthorized, restError(500, "Not Implemented"))
+          }
+        }
+      }
+    }
+  }
+
+  def sanity(claim: OAuth2Claim): Route = {
+    path("sanity") {
+      complete(StatusCodes.OK, restSuccess(0, JsObject(
+        "identifier" -> JsString(claim.identifier)
+      )))
+    }
+  }
+
+  def refresh: Route = {
+    path("refresh") {
+      post {
+        entity(as[LoginRefreshRequest]) { refreshReq =>
+          context.oauth2.refreshToken(refreshReq.refresh_token) match {
+            case Some(OAuth2RefreshClaim(identifier, attributes)) =>
+              val previousValue = attributes("issuer") // re-use issuer value from refresh token
+              //logger.info(s"============> attributes issuer='$previousValue'")
+              val newClaim = OAuth2Claim(identifier, Map("issuer" -> previousValue, "refreshed" -> "true"))
+              context.oauth2.reissueJwt(newClaim, refreshReq.refresh_token) { jwt =>
+                val response = LoginRefreshResponse(jwt.tokenType, jwt.accessToken, jwt.accessTokenExpire, jwt.refreshToken, jwt.refreshTokenExpire)
+                complete(StatusCodes.OK, restSuccess(0, response.toJson))
+              }
+            case _ =>
+              complete(StatusCodes.Unauthorized, restError(401, "Invalid refresh token"))
+          }
+        }
+      }
+    }
+  }
+}
